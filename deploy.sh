b374fcf39436f80bf709c11ffeff3d2d7e533ea6
Your Name <you@example.com>
2020-11-01 23:47:27 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 GIT_USER=richardgong1987 USE_SSH=true yarn run deploy
 ssh root@161.117.178.164 << eeooff
 
-rm -fr /usr/share/nginx/html/*
+cd  -fr /usr/share/nginx/html/
 
-cp -fr /root/google-microsoft.com/build/* /usr/share/nginx/html/
+git pull
 
 eeooff
 
